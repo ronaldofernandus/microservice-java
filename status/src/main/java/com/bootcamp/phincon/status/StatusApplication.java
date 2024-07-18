8f593f837b9ee4cb2d322942947e7dde1812db3e
ronaldofernandus <ronaldofernandus10.com>
2024-07-18 12:52:12 +0700

    feat: add main app

---
+++ status/src/main/java/com/bootcamp/phincon/status/StatusApplication.java
@@ -0,0 +1,21 @@
+package com.bootcamp.phincon.status;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJms
+@EnableScheduling
+
+@OpenAPIDefinition(info = @Info(title = "Swagger ",version = "1.0",description = "Documentation APIs v1.0"))
+public class StatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusApplication.class, args);
+	}
+
+}
